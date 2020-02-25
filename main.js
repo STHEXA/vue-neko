684c62bd34e5f510c60edeb9e2818784858b9e9a
STHEXA <ti.syu.s@gmail.com>
2020-02-25 20:14:25 +0900

    no90

--- main.js
+++ main.js
@@ -21,29 +21,52 @@ var app = new Vue({
   // data: {
   //   ok: false
   // }
-  data: {
-    name: 'キマイラ',
-    list: [
-      {id: 1, name: 'スライム', hp: 100},
-      {id: 2, name: 'ゴブリン', hp: 200},
-      {id: 3, name: 'ドラゴン', hp: 500}
-    ]
+  // data: {
+  //   name: 'キマイラ',
+  //   list: [],
+  //   text: 'Vue'
+  // },
+  // methods: {
+  //   doAdd: function() {
+  //     var max = this.list.reduce(function(a,b) {
+  //       return a > b.id ? a: b.id
+  //     },0) 
+  //     this.list.push({
+  //       id: max + 1,
+  //       name: this.name,
+  //       hp: 500
+  //     })
+  //   },
+  //   doRemove: function(index) {
+  //     this.list.splice(index, 1)
+  //   },
+  //   doAttack: function(index) {
+  //     this.list[index].hp -= 10
+  //   }
+  // },
+  // created: function() {
+  //   axios.get('list.json').then(function(response) {
+  //     this.list = response.data
+  //   }.bind(this)).catch(function(e) {
+  //     console.error(e)
+  //   })
+  // }
+  mounted: function() {
+    console.log(this.$el)
+    console.log(this.$refs.hello)
   },
-  methods: {
-    doAdd: function() {
-      var max = this.list.reduce(function(a,b) {
-        return a > b.id ? a: b.id
-      },0) 
-      this.list.push({
-        id: max + 1,
-        name: this.name,
-        hp: 500
-      })
-    },
-    doRemove: function(index) {
-      this.list.splice(index, 1)
-    }
-    
+
+  data: {
+    // あらかじめ空リストを用意しておく
+    list: []
+  },
+  created: function () {
+    axios.get('list.json').then(function (response) {
+      // 取得完了したらlistリストに代入
+      this.list = response.data
+    }.bind(this)).catch(function (e) {
+      console.error(e)
+    })
   }
 
 })
